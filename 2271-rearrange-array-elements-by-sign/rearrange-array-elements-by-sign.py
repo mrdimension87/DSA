e20281b0ca918488a419caa96536a34e6755b619
Adirya Kumar <mr.dimension1087@gmail.com>
2025-07-03 12:03:48 +0530

    Time: 100 ms (53.76%) | Memory: 44.7 MB (67.35%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -4,14 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        pos=[]
-        neg=[]
+        po=[]
+        ne=[]
         for i in nums:
             if(i>0):
-                pos.append(i)
+                po.append(i)
             else:
-                neg.append(i)
+                ne.append(i)
         for i in range(len(nums)//2):
-            nums[2*i]=pos[i]
-            nums[2*i+1]=neg[i]
+            nums[2*i]=po[i]
+            nums[2*i+1]=ne[i]
         return nums
