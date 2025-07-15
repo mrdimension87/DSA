25eb5c3c1c9028dbbd9ff4fd71e35d4f41fc8fe1
Aditya Kumar <mr.dimension1087@gmail.com>
2025-07-15 18:35:36 +0530

    Time: 13 ms (11.94%) | Memory: 24.6 MB (5.37%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int> cnt;
+        int n=nums.size();
+        for(int i=0; i<n; i++){
+            cnt[nums[i]]++;
+        }
+        for(auto it: cnt){
+            if(it.second == 1){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+
+};
