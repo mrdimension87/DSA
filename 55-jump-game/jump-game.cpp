2f028b45305abe57ef24036e7955796b4925b22e
Aditya Kumar <mr.dimension1087@gmail.com>
2025-08-18 15:51:55 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (34.08%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int target = n - 1;  
+        for (int i = n - 2; i >= 0; i--) {
+            if (i + nums[i] >= target) {
+                target = i;  // shift target
+            }
+        }
+        if(target==0){
+            return true;
+        }else{ 
+            return false;
+        }
+    }
+};
