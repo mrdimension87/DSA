6a870a48b5688929930c53b81234a4be9de81afa
Aditya Kumar <mr.dimension1087@gmail.com>
2026-01-02 22:46:02 +0530

    Time: 3 ms (13.70%) | Memory: 9.6 MB (7.43%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    const char findTheDifference(string s, string t) {
+        map<char,int>mp;
+        for(auto it : s){
+            mp[it]++;
+        }
+        map<char,int> mp1;
+        for(auto it : t){
+            mp1[it]++;
+        }
+        for(auto it: t){
+            if(mp[it]!=mp1[it]){
+                return it;
+            }
+        }
+        char x;
+        return x;
+    }
+};
