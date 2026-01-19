412bf8630fbaf3d74cc5ee9c880946f76441b852
Aditya Kumar <mr.dimension1087@gmail.com>
2026-01-19 19:01:11 +0530

    Time: 1588 ms (5.34%) | Memory: 73.9 MB (99.93%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n= nums.size();
+        
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<i+1+k; j++){
+                if(j>=n){
+                    break;
+                }
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
