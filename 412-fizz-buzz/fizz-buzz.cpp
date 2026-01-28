5726503c7349e51954c82dd3ae7b8b9efd848d13
Aditya Kumar <mr.dimension1087@gmail.com>
2026-01-28 23:50:10 +0530

    Time: 3 ms (32.13%) | Memory: 11.7 MB (43.09%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        for(int i = 1; i < n+1; i++){
+            if(i%15 == 0){
+                ans.push_back("FizzBuzz");
+                continue;
+            }
+            if(i%5== 0 ){
+                ans.push_back("Buzz");
+                continue;
+            }
+            if(i%3 == 0){
+                ans.push_back("Fizz");
+                continue;
+            }
+            ans.push_back(to_string(i));
+        }     
+
+        return ans;  
+    }
+};
